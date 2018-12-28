b74cfe164559e63f15f32e65396a32e18e28dc79
asrsistemas <andersonsilva.cps@gmail.com>
2018-12-28 01:11:45 -0200

    criação de telas

---
+++ app/src/main/java/asrdelivery/com/br/asrdelivery/TelaMontagemPedido.java
@@ -0,0 +1,17 @@
+package asrdelivery.com.br.asrdelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class TelaMontagemPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_montagem_pedido);
+
+
+    }
+}
