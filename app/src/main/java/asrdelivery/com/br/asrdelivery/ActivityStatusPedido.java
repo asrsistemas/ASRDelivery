2b9b6c1f1252ce1e8d11eae203b04a5b3c132d89
asrsistemas <andersonsilva.cps@gmail.com>
2019-01-02 16:49:03 -0200

    Construção de telas

--- app/src/main/java/asrdelivery/com/br/asrdelivery/ActivityStatusPedido.java
+++ app/src/main/java/asrdelivery/com/br/asrdelivery/ActivityStatusPedido.java
@@ -1,7 +1,9 @@
 package asrdelivery.com.br.asrdelivery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityStatusPedido extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ActivityStatusPedido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_pedido);
+    }
+    public void sair(View view) {
+        Intent intentSair = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intentSair);
     }
 }
